7b5cbb96bf4fe7077466288c023d452bdffe3035
Bruce Szalwinski <bruce.szalwinski@cdk.com>
2017-04-21 23:34:59 -0500

    add helpers

---
+++ src/test/java/liquibase/ext/helpers/SetUtils.java
@@ -0,0 +1,20 @@
+package liquibase.ext.helpers;
+
+import java.util.Set;
+
+public class SetUtils {
+
+    public static boolean equals(Set<?> set1, Set<?> set2){
+
+        if(set1 == null || set2 ==null){
+            return false;
+        }
+
+        if(set1.size()!=set2.size()){
+            return false;
+        }
+
+        return set1.containsAll(set2);
+
+    }
+}
