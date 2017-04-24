86416b3ba717a443fc5c9089f833655ceab4294a
Bruce Szalwinski <bruce.szalwinski@cdk.com>
2017-04-24 10:09:14 -0500

    add a few more tests

--- src/test/java/liquibase/ext/datatype/TimestampTypeTest.java
+++ src/test/java/liquibase/ext/datatype/TimestampTypeTest.java
@@ -1,13 +1,19 @@
 package liquibase.ext.datatype;
 
 import liquibase.database.core.PostgresDatabase;
+import liquibase.datatype.DataTypeFactory;
 import liquibase.datatype.DatabaseDataType;
+import liquibase.datatype.LiquibaseDataType;
 import liquibase.ext.database.SnowflakeDatabase;
 import org.junit.Before;
 import org.junit.Test;
 
 import static liquibase.servicelocator.PrioritizedService.PRIORITY_DATABASE;
-import static org.junit.Assert.*;
+import static org.hamcrest.collection.IsArrayContaining.hasItemInArray;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class TimestampTypeTest {
 
@@ -26,7 +32,21 @@ public class TimestampTypeTest {
         assertEquals("TIMESTAMP_LTZ", databaseDataType.getType());
         assertEquals("TIMESTAMP_LTZ", databaseDataType.toSql());
         assertFalse(databaseDataType.isAutoIncrement());
+    }
 
+    @Test
+    public void timestampAliases() throws Exception {
+        LiquibaseDataType liquibaseDataType = DataTypeFactory.getInstance().fromDescription("datetime", snowflakeDatabase);
+        String[] aliases = liquibaseDataType.getAliases();
+        assertEquals(2, aliases.length);
+        assertThat(aliases, hasItemInArray("datetime"));
+        assertThat(aliases, hasItemInArray("java.sql.Types.DATETIME"));
+    }
+
+    @Test
+    public void datetimeConvertsToTimestamp() throws Exception {
+        LiquibaseDataType liquibaseDataType = DataTypeFactory.getInstance().fromDescription("datetime", snowflakeDatabase);
+        assertEquals("liquibase.ext.datatype.TimestampType", liquibaseDataType.getClass().getName());
     }
 
     @Test
@@ -38,7 +58,16 @@ public class TimestampTypeTest {
     @Test
     public void getPriority() throws Exception {
         assertEquals(PRIORITY_DATABASE, timestampType.getPriority());
+    }
 
+    @Test
+    public void getMinParameters() throws Exception {
+        assertEquals(0, timestampType.getMinParameters(snowflakeDatabase));
+    }
+
+    @Test
+    public void getMaxParameters() throws Exception {
+        assertEquals(0, timestampType.getMinParameters(snowflakeDatabase));
     }
 
 }
